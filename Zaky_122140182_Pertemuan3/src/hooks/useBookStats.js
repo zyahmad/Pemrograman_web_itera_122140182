9d9c0b71db907db3ece3842841ff8bb8059dd1ef
Zaky Ahmad <zyahmadm@gmail.com>
2025-04-20 23:24:57 +0700

    Update useBookStats.js

--- Zaky_122140182_Pertemuan3/src/hooks/useBookStats.js
+++ Zaky_122140182_Pertemuan3/src/hooks/useBookStats.js
@@ -3,11 +3,11 @@ import { useMemo } from 'react';
 function useBookStats(books) {
   return useMemo(() => {
     const total = books.length;
-    const owned = books.filter(book => book.status === 'milik').length;
-    const reading = books.filter(book => book.status === 'baca').length;
-    const toBuy = books.filter(book => book.status === 'beli').length;
+    const milik = books.filter(book => book.status === 'milik').length;
+    const baca = books.filter(book => book.status === 'baca').length;
+    const beli = books.filter(book => book.status === 'beli').length;
 
-    return { total, owned, reading, toBuy };
+    return { total, milik, baca, beli };
   }, [books]);
 }
 
